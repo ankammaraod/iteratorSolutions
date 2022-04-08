d64e7aa32bde28b8a840ee9c758e1dc565839d35
Ankammarao D <99481377+ankammaraod@users.noreply.github.com>
2022-04-08 12:00:59 +0530

    Refactored identifiers

--- generateMultiples.js
+++ generateMultiples.js
@@ -10,31 +10,23 @@ const generateArray = function (count) {
 
 const multiplyWith = function (x) {
   return function (num) {
-    return (num) * x;
+    return num * x;
   }
 };
-
-// const generateRow = function (multiplier, multiples) {
-// const table = [];
-// for (let index = 1; index <= multiples.length; index++)
-// table.push(multiplier + "*" + index + "=" + multiples[index - 1]);
-// return table;
-// };
 
 const generateRow = function (multiplier) {
   return function (multiple, index) {
-    return multiplier + "*" + (index + 1) + "=" + multiple;
+    return multiplier + " * " + (index + 1) + " = " + multiple;
   }
 };
 
-const generateMultiple = function (multiplier, count) {
+const generateTable = function (multiplier, count) {
   const array = generateArray(count);
 
-  const multiplyWithFive = multiplyWith(multiplier);
-  const multiples = array.map(multiplyWithFive);
+  const multiples = array.map(multiplyWith(multiplier));
 
-  const generateTable = generateRow(multiplier);
-  return multiples.map(generateTable);
+  return multiples.map(generateRow(multiplier));
 };
 
-console.log(generateMultiple(5, 10));
+console.log(generateTable(5, 10));
+console.log(generateTable(2, 10));
