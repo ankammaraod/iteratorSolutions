74bc20805e027f9ce99562991831a31f81391bdb
Ankammarao D <99481377+ankammaraod@users.noreply.github.com>
2022-04-11 12:41:31 +0530

    removed console.log

--- sort.js
+++ sort.js
@@ -1,8 +1,6 @@
 const sort = function (elements) {
-  i = 0;
   for (let outer = 0; outer < elements.length; outer++) {
     for (let inner = outer + 1; inner < elements.length; inner++) {
-      i++;
       if (elements[inner] < elements[outer]) {
         const smallest = elements[inner];
         elements[inner] = elements[outer];
@@ -10,10 +8,8 @@ const sort = function (elements) {
       }
     }
   }
-  console.log(i);
   return elements;
 };
 
 console.log(sort([3, 4, 1, 2]));
-console.log(sort([1, 2, 3, 4]));
-console.log(sort([3, 4, 4, 2, 1]));
+console.log(sort([3, 4, 5, 2, 1]));
