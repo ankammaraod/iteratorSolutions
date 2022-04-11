1f23af23b2e4a62bec82b7d270b5a9cbe66f12f5
Ankammarao D <99481377+ankammaraod@users.noreply.github.com>
2022-04-11 12:23:38 +0530

    made efficiency

--- sort.js
+++ sort.js
@@ -1,6 +1,8 @@
 const sort = function (elements) {
+  i = 0;
   for (let outer = 0; outer < elements.length; outer++) {
-    for (let inner = outer; inner < elements.length; inner++) {
+    for (let inner = outer + 1; inner < elements.length; inner++) {
+      i++;
       if (elements[inner] < elements[outer]) {
         const smallest = elements[inner];
         elements[inner] = elements[outer];
@@ -8,8 +10,10 @@ const sort = function (elements) {
       }
     }
   }
+  console.log(i);
   return elements;
 };
 
 console.log(sort([3, 4, 1, 2]));
+console.log(sort([1, 2, 3, 4]));
 console.log(sort([3, 4, 4, 2, 1]));
